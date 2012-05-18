7cae40490a409fce7a078d9bd1ec4f60c997bcf9
IP MYUNG JIN <jsveron23@gmail.com>
2012-05-18 11:43:45 +0900

    add guide

--- app/view/Main.js
+++ app/view/Main.js
@@ -16,6 +16,23 @@ Ext.define('ToolbarSpinner.view.Main', {
 					hidden: true
 				}
 			}, {
+				xtype: 'container',
+				html : [
+					'<h3>How to use:</h3>',
+					'<code>',
+						'items: {<br />',
+						'&nbsp;&nbsp;docked: \'top\',<br />',
+						'&nbsp;&nbsp;xtype : \'titlebar\'<br />',
+						'&nbsp;&nbsp;title : \'ToolbarSpinner\'<br />',
+						'&nbsp;&nbsp;items : {<br />',
+						'&nbsp;&nbsp;&nbsp;&nbsp;xtype : \'tbarspinner\'<br />',
+						'&nbsp;&nbsp;&nbsp;&nbsp;align : \'right\'<br />',
+						'&nbsp;&nbsp;}<br />',
+						'}<br />',
+					'</code>'
+				].join(''),
+				styleHtmlContent: true
+			}, {
 				xtype: 'button',
 				text : 'Show Spinner',
 				id   : 'ToggleSpinner'
